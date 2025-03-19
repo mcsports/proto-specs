8a044849c1163c569b7202ccfc226c5a5790781a
dayyeeet <david@cappell.net>
2025-03-19 11:04:31 +0100

    fix: include proto java into proto kotlin

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":proto-java"))
+    api(project(":proto-java"))
     implementation("com.google.protobuf:protobuf-kotlin:4.30.1")
     implementation("io.grpc:grpc-protobuf:1.71.0")
     implementation("io.grpc:grpc-stub:1.71.0")
